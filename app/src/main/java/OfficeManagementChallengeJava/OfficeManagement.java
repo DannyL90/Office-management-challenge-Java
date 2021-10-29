b307616cafd7ca73853299c24d8906f3f1b7cf28
Daniel <daniel.szabo09@gmail.com>
2021-10-29 12:22:00 +0100

    OfficeManagement class added

---
+++ app/src/main/java/OfficeManagementChallengeJava/OfficeManagement.java
@@ -0,0 +1,5 @@
+package OfficeManagementChallengeJava;
+
+public class OfficeManagement {
+
+}
---
+++ app/src/test/java/OfficeManagementChallengeJava/OfficeManagementTest.java
@@ -0,0 +1,13 @@
+package OfficeManagementChallengeJava;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class OfficeManagementTest {
+
+    @Test
+    public void testOfficeManagementClass() {
+        OfficeManagement newOfficeManagement = new OfficeManagement();
+
+    }
+}
